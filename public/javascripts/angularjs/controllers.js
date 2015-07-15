f68965621317e7a38e36d1bc61bdad39006714d3
rffreedom <873625689@qq.com>
2015-07-15 18:13:26 +0800

    gg

--- public/javascripts/angularjs/controllers.js
+++ public/javascripts/angularjs/controllers.js
@@ -16,6 +16,62 @@ function drawMarkers(map, posts) {
       offset:{x:-8, y:-34}
     });
     marker.setTitle(post.text);
+    AMap.event.addListener(marker,'click',function(){
+       infoWindow.open(map,marker.getPosition());
+     });
+     //实例化信息窗体
+		var infoWindow = new AMap.InfoWindow({
+				isCustom:true,  //使用自定义窗体
+				content:createInfoWindow('方恒假日酒店&nbsp;&nbsp;<span style="font-size:11px;color:#F00;">价格:318</span>',"<img src='http://tpc.googlesyndication.com/simgad/5843493769827749134' style='position:relative;float:left;margin:0 5px 5px 0;'>地址：北京市朝阳区阜通东大街6号院3号楼 东北 8.3 公里<br/>电话：010 64733333<br/><a href='http://baike.baidu.com/view/6748574.htm'>详细信息</a>"),
+				offset:new AMap.Pixel(16, -45)//-113, -140
+			});
+
+		//构建自定义信息窗体
+		function createInfoWindow(title,content){
+			var info = document.createElement("div");
+			info.className = "info";
+
+			//可以通过下面的方式修改自定义窗体的宽高
+			//info.style.width = "400px";
+
+			// 定义顶部标题
+			var top = document.createElement("div");
+			var titleD = document.createElement("div");
+			var closeX = document.createElement("img");
+			top.className = "info-top";
+			titleD.innerHTML = title;
+			closeX.src = "http://webapi.amap.com/images/close2.gif";
+			closeX.onclick = closeInfoWindow;
+
+			top.appendChild(titleD);
+			top.appendChild(closeX);
+			info.appendChild(top);
+
+
+			// 定义中部内容
+			var middle = document.createElement("div");
+			middle.className = "info-middle";
+			middle.style.backgroundColor='white';
+			middle.innerHTML = content;
+			info.appendChild(middle);
+
+			// 定义底部内容
+			var bottom = document.createElement("div");
+			bottom.className = "info-bottom";
+			bottom.style.position = 'relative';
+			bottom.style.top = '0px';
+			bottom.style.margin = '0 auto';
+			var sharp = document.createElement("img");
+			sharp.src = "http://webapi.amap.com/images/sharp.png";
+			bottom.appendChild(sharp);
+			info.appendChild(bottom);
+			return info;
+		}
+
+		//关闭信息窗体
+		function closeInfoWindow(){
+			map.clearInfoWindow();
+		}
     markers.push(marker);
   });
 
